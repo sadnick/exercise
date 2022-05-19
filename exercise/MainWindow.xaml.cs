7817a03e58fbb26aa339736014f4ab68d9cd9e0a
sadnick <sadyrinnk@whatever.ru>
2022-05-19 11:33:42 +0300

    update

--- exercise/MainWindow.xaml.cs
+++ exercise/MainWindow.xaml.cs
@@ -25,7 +25,18 @@ namespace exercise
             InitializeComponent();
         }
 
-        
+        private void textbox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+
+        private void TextBox2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             var num1 = Convert.ToDouble(textbox1.Text);
@@ -34,7 +45,24 @@ namespace exercise
             Answer.Text = answer.ToString();
         }
 
-      
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            var num1 = Convert.ToDouble(textbox1.Text);
+            var num2 = Convert.ToDouble(textbox2.Text);
+
+            if (num2 == 0)
+
+            {
+                Answer.Text = "Обнаружено деление на ноль";
+            }
+            else
+            {
+                var answer = num1 / num2;
+                Answer.Text = answer.ToString();
+            }
+        }
+
+
     }
 }
 
