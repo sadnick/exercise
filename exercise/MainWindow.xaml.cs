a64459a7b85ff1bdbff2378ce10ae1a5521dcba6
sadnick <sadyrinnk@whatever.ru>
2022-05-19 11:15:32 +0300

    Добавьте файлы проекта.

---
+++ exercise/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace exercise
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var num1 = Convert.ToDouble(textbox1.Text);
+            var num2 = Convert.ToDouble(textbox2.Text);
+            var answer = num1 + num2;
+            Answer.Text = answer.ToString();
+        }
+
+      
+    }
+}
+
